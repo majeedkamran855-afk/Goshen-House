580b2770e201843e0a9cee5925057b1f34a3756d
majeedkamran855-afk <majeedkamran855@gmail.com>
2026-09-07 00:28:45 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -108,7 +108,7 @@ export const Navbar: React.FC = () => {
           {/* Action CTAs */}
           <div className="hidden lg:flex items-center gap-3">
             <a
-              href="tel:+1 832-762-6156"
+              href="tel:+1 832-770-6831"
               className="text-xs font-semibold px-3 py-2 text-foreground/70 hover:text-primary flex items-center gap-1.5 transition-colors"
             >
               <svg
@@ -124,7 +124,7 @@ export const Navbar: React.FC = () => {
                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                 />
               </svg>
-              +1 832-762-6156
+              +1 832-770-6831
             </a>
             <Link
               to="/forms"
@@ -240,7 +240,7 @@ export const Navbar: React.FC = () => {
                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                 />
               </svg>
-              Call +1 832-762-6156
+              Call +1 832-770-6831
             </a>
             <Link
               to="/forms"
